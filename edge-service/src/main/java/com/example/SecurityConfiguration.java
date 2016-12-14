aa22be489977343be9c43337c142339817a2d317
Matt Raible <matt@raibledesigns.com>
2016-12-14 11:47:10 -0700

    Lock it all down!

--- edge-service/src/main/java/com/example/SecurityConfiguration.java
+++ edge-service/src/main/java/com/example/SecurityConfiguration.java
@@ -10,8 +10,6 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.apply(stormpath()).and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
+        http.apply(stormpath());
     }
 }
