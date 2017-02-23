f391704356551fa66beecd77f4efefe78c1e2f2e
Matt Raible <matt@raibledesigns.com>
2017-02-23 13:08:00 -0500

    Fixed deployment to CF

--- deploy.sh
+++ deploy.sh
@@ -25,8 +25,6 @@ function deploy_service(){
     cf create-user-provided-service $N -p $JSON
 }
 
-
-
 ### installation
 
 cd `dirname $0`
@@ -48,14 +46,23 @@ cd $r && find . -iname pom.xml | xargs -I pom  mvn -DskipTests clean install -f 
 
 # client first
 cd $r/client
-#rm -rf node_modules
+rm -rf dist
 npm install && ng build --prod --aot
 python $r/sw.py
 cd dist
 touch Staticfile
 cf push pwa-client --no-start --random-route
 cf set-env pwa-client FORCE_HTTPS true
-cf pwa-client start
+cf start pwa-client
+
+# Get the URL for the client
+clientUri=https://`app_domain pwa-client`
+
+# replace the client URL in the server
+sed -i -e "s|http://localhost:4200|$clientUri|g" $r/edge-service/src/main/resources/application.properties
+# repackage the edge-service
+cd $r/edge-service
+mvn package -DskipTests
 
 # Eureka
 cd $r/eureka-service
@@ -68,15 +75,21 @@ cf push -p target/*jar pwa-beer-catalog-service --no-start  --random-route
 cf bs pwa-beer-catalog-service  pwa-eureka-service
 cf start pwa-beer-catalog-service
 
-
 # Edge Service
 stormpathApiKeyId=`cat ~/.stormpath/apiKey.properties | grep apiKey.id | cut -f3 -d\ `
 stormpathApiKeySecret=`cat ~/.stormpath/apiKey.properties | grep apiKey.secret | cut -f3 -d\ `
 
 cd $r/edge-service
-cf push -p target/*jar pwa-edge-service --no-start  -n pwa-edge-service
+cf push -p target/*jar pwa-edge-service --no-start -n pwa-edge-service
 cf set-env pwa-edge-service STORMPATH_API_KEY_ID $stormpathApiKeyId
 cf set-env pwa-edge-service STORMPATH_API_KEY_SECRET $stormpathApiKeySecret
-cf set-env pwa-edge-service PWA_CLIENT_URI https://`app_domain pwa-client`
+#cf set-env pwa-edge-service PWA_CLIENT_URI https://`app_domain pwa-client`
 cf bs pwa-edge-service pwa-eureka-service
 cf start pwa-edge-service
+
+# cleanup changed files
+git checkout $r/edge-service
+rm $r/edge-service/src/main/resources/application.properties-e
+
+# show apps and URLs
+cf apps
