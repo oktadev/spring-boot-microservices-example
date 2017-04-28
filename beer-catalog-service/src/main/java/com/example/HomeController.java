319d6d4b690b449376092da5cebe993745325de0
Matt Raible <matt@raibledesigns.com>
2017-04-28 12:55:22 -0600

    Fix mapping for /home so it renders the user's information

--- beer-catalog-service/src/main/java/com/example/HomeController.java
+++ beer-catalog-service/src/main/java/com/example/HomeController.java
@@ -3,6 +3,7 @@ package com.example;
 import org.juiser.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
