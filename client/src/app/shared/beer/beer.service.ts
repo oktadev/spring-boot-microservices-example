4aee2039b60d240b5a869ad7c2c2754112b023c2
Matt Raible <matt.raible@okta.com>
2018-01-18 17:54:13 -0700

    Fix BeerService

--- client/src/app/shared/beer/beer.service.ts
+++ client/src/app/shared/beer/beer.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { OktaService } from '../okta/okta.service';
 
 @Injectable()
 export class BeerService {
 
-  constructor(private http: HttpClient, private oktaService: OktaAuthService) {
+  constructor(private http: HttpClient, private oktaService: OktaService) {
   }
 
   getAll(): Observable<any> {
-    const headers: Headers = new Headers();
-    if (this.oktaService.isAuthenticated()) {
-      const accessToken = this.oktaService.signIn.tokenManager.get('accessToken');
-      headers.append('Authorization', accessToken.tokenType + ' ' + accessToken.accessToken);
+    let headers: HttpHeaders = new HttpHeaders();
+    if (this.oktaService.getAccessToken()) {
+      const accessToken = this.oktaService.getAccessToken();
+      // headers is immutable, so re-assign
+      headers = headers.append('Authorization', accessToken.tokenType + ' ' + accessToken.accessToken);
     }
-    return this.http.get('http://localhost:8081/good-beers',
-      {headers: new HttpHeaders().set()});
+    return this.http.get('http://localhost:8081/good-beers', {headers: headers});
   }
 }
