aa2e208fcc4aecb041f1698f31e25664842b6370
Matt Raible <matt.raible@okta.com>
2018-01-23 14:16:51 -0700

    Lock down catalog service, configure feign to talk to it

--- edge-service/src/main/java/com/example/ResourceServerConfiguration.java
+++ edge-service/src/main/java/com/example/ResourceServerConfiguration.java
@@ -13,7 +13,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
             .requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
             .authorizeRequests()
             .antMatchers("/**").authenticated();
