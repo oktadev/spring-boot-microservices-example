a8768c5a7b4cac14908c0def2df32bee92c93e17
Chuck D'Antonio <c_dantonio@acm.org>
2019-01-28 12:25:57 -0500

    Update to use a non-default resoure_id so calls will succeed

--- beer-catalog-service/src/main/java/com/example/beercatalogservice/ResourceServerConfig.java
+++ beer-catalog-service/src/main/java/com/example/beercatalogservice/ResourceServerConfig.java
@@ -1,14 +1,24 @@
 package com.example.beercatalogservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${openid.resourceId}")
+    String resource_id ;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resource_id);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
